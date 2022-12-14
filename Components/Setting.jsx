12405f4969edf62b6f87c1adc4b776f0dcad39c3
Ammar <ammarfarghani@gmail.com>
2022-12-14 20:34:02 +0700

    rmv header in home

--- Components/Setting.jsx
+++ Components/Setting.jsx
@@ -1,13 +1,16 @@
 import { View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Setting() {
   const navigation = useNavigation()
 
   return (
-    <View className='flex-row justify-end'>
-      <TouchableOpacity className='bg-gray-300 p-1.5' onPress={() => navigation.navigate('Setting')}></TouchableOpacity>
-    </View>
+    <SafeAreaView>
+      <View className='flex-row justify-end'>
+        <TouchableOpacity className='bg-gray-300 p-1.5' onPress={() => navigation.navigate('Setting')}></TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
